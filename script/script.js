3c75fbf1f73e11aa2db250c49c2b3cf942c0f0e9
Terraluminiscentia <cumabacafypdoang@gmail.com>
2025-05-16 21:43:57 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.getElementById('mobile-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.getElementById('mobile-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+  menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+  });
+});
